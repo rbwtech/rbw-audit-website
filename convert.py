626200aaa1ec134f7697a1eeb292f86d3f5265c7
rbwtech <radipta111@gmail.com>
2025-10-09 16:30:29 +0700

    Initial commit: Safe Browsing Checker

--- convert.py
+++
@@ -1,6 +0,0 @@
-from cairosvg import svg2png
-from PIL import Image
-
-svg2png(url='icon.svg', write_to='icon.png')
-img = Image.open('icon.png')
-img.save('icon.ico', format='ICO', sizes=[(256, 256)])
